b8403e3c55c152c2b8228481bd92335b971f802a
Martinha22 <marthalocks.libro@gmail.com>
2022-06-19 13:20:05 -0300

    Exercicio 1

--- numbers.py
+++ numbers.py
@@ -5,6 +5,7 @@ os numeros menores ou iguais a um determinado numero
 """
 x = input("Escreva um numero inteiro maior que 0: ")
 y = 0
+
 for i in range(1, int(x) + 1):
     y = y + i
     print('i=', i, '\ty=', y)
