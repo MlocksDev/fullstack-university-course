bff7d2411838a74f7dc75e2f686e1bea51b019d4
Martinha22 <marthalocks.libro@gmail.com>
2022-06-19 13:05:05 -0300

    primeiro exercicio em python da faculdade

---
+++ numbers.py
@@ -0,0 +1,12 @@
+"""
+Exercicio 1
+Implementacao do algortimo para imprimir e somar todos
+os numeros menores ou iguais a um determinado numero
+"""
+x = input("Escreva um numero: ")
+y = 0
+for i in range(1, int(x) + 1):
+    y = y + i
+    print('i=', i, '\ty=', y)
+
+print('Total=', y)
