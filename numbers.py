c232c7f093ddac9a298345c112748d883f1ba90a
Martinha22 <marthalocks.libro@gmail.com>
2022-06-19 13:14:04 -0300

    ajuste no enunciado

--- numbers.py
+++ numbers.py
@@ -3,7 +3,7 @@ Exercicio 1
 Implementacao do algortimo para imprimir e somar todos
 os numeros menores ou iguais a um determinado numero
 """
-x = input("Escreva um numero: ")
+x = input("Escreva um numero inteiro maior que 0: ")
 y = 0
 for i in range(1, int(x) + 1):
     y = y + i
